8982962f24e32d5d12dd7d9724575bd4bf11c0b9
aqoudayo <hiyeonu0520@gmail.com>
2024-04-10 03:37:36 +0900

    [inital] Test Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.listen(8080, function() {
+    console.log('listening on 8080')
+})
+
+app.get('/', function(request, reply){
+    reply.sendFile(path.join(__dirname, 'dancecomm/public/index.html'))
+})
